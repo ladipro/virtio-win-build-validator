44e74fee70c3513e7dc00bd8ae527360e1eacd79
Ladi Prosek <lprosek@redhat.com>
2016-09-13 13:50:31 +0200

    Make InfFileComparer aware of line endings
    If two .inf files differ only in the line endings they use
    (LF vs. CR, LF) InfFileComparer would currently consider them
    identical. This commit adds an explicit output describing
    the line endings detected in the file to help uncover issues
    where the intended line endings haven't survived git push.
    
    "File uses CR LF line endings."
    or
    "File uses LF line endings."
    or
    "Failed to detect line endings in this file."

--- Comparers/InfFileComparer.cs
+++ Comparers/InfFileComparer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BuildValidator
@@ -7,12 +8,41 @@ namespace BuildValidator
     {
         protected async override Task Preprocess(string infile, string outfile)
         {
+            // determine/guess whether the file uses Unix (LF) or Windows (CR, LF) line endings
+            char[] buffer = new char[1024];
+            char ending = '\0';
+            using (StreamReader sr = new StreamReader(infile))
+            {
+                int read;
+                do
+                {
+                    read = sr.ReadBlock(buffer, 0, buffer.Length);
+                    ending = Array.Find(buffer, ch => (ch == '\r' || ch == '\n'));
+                    if (ending != '\0')
+                    {
+                        break;
+                    }
+                }
+                while (read == buffer.Length);
+            }
+
+            using (StreamWriter sw = new StreamWriter(outfile))
+            {
+                switch (ending)
+                {
+                    case '\r': sw.WriteLine("File uses CR LF line endings."); break;
+                    case '\n': sw.WriteLine("File uses LF line endings."); break;
+                    default: sw.WriteLine("Failed to detect line endings in this file."); break;
+                }
+                sw.WriteLine();
+            }
+
             var cmd = new FileSource(infile)
                 // remove comments, remove trailing whitespace, remove empty lines
                 .Pipe(new Command(Tools.Sed, @"s/\(;.*\)$//;s/\(\s*\)$//;/^\s*$/d"))
                 // normalize date/version
                 .Pipe(new Command(Tools.Sed, @"s/DriverVer=[^,]*,\([^\.]*\.[^\.]*\.[^\.]*\)\..*$/DriverVer=00\/00\/0000,\1.0/"))
-                .Pipe(new FileSink(outfile));
+                .Pipe(new FileSink(outfile, FileMode.Append));
             await cmd.Run();
         }
     }
